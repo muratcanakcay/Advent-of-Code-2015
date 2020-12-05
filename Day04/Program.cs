9a0a3b21eb90870a8e5ff9ec668abd4f668485b9
Muratcan Akçay <muratcanakcay@gmail.com>
2020-12-05 20:35:00 +0100

    Init

---
+++ Day04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+internal class Day04
+{
+    private static void Main()
+    {
+        const int NUM_ZEROS = 5;       // make it 6 for part 2   
+
+        string puzzleinput = "iwrupvqb";
+        bool found = false;
+        int num = 0;
+
+        while (!found)
+        {
+            num++;
+            var source = puzzleinput + num;
+
+            // Code taken from: https://riptutorial.com/csharp/example/9341/md5
+            // Creates an instance of the default implementation of the MD5 hash algorithm.
+            using (var md5Hash = MD5.Create())
+            {
+                // Byte array representation of source string
+                var sourceBytes = Encoding.UTF8.GetBytes(source);
+
+                // Generate hash value(Byte Array) for input data
+                var hashBytes = md5Hash.ComputeHash(sourceBytes);
+
+                // Convert hash byte array to string
+                var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+
+                // Output the MD5 hash
+                //Console.WriteLine("The MD5 hash of " + source + " is: " + hash);    // uncomment if you want to see all trials
+
+                found = true;
+                for (int i = 0; i < NUM_ZEROS; i++)               
+                    if (hash[i] != '0') found = false; 
+                
+                if(found) Console.WriteLine("The MD5 hash of " + source + " is: " + hash); 
+            }
+        }
+    }
+}
